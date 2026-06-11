0b0f8451971de2a724599d24bb0f6f28ac5c6140
lunarmagnoly <mari.luna.tei@outlook.com>
2026-06-11 06:34:22 +0300

    Set up Express server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,21 @@
-// Express server will be configured here
+import express from "express";
+import cors from "cors";
+
+// Loome Express rakenduse
+const app = express();
+
+// Lubab frontendil teha päringuid backendile
+app.use(cors());
+
+// Lubab serveril lugeda JSON formaadis andmeid
+app.use(express.json());
+
+// Test route, et kontrollida kas backend töötab
+app.get("/", (req, res) => {
+  res.send("CodeCat API töötab");
+});
+
+// Server hakkab töötama pordil 4000
+app.listen(4000, () => {
+  console.log("Server töötab pordil 4000");
+});
