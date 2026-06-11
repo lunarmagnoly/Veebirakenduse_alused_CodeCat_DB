3a2abfad3c0068a4643217b3dfbfd017f107a783
lunarmagnoly <mari.luna.tei@outlook.com>
2026-06-11 18:42:52 +0300

    Implement GET tasks endpoint

--- server/routes/tasks.ts
+++ server/routes/tasks.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import sql from "mssql/msnodesqlv8";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Tasks route works" });
+router.get("/", async (req, res) => {
+  try {
+    const result = await sql.query("SELECT * FROM Tasks");
+
+    res.json(result.recordset);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Andmete laadimine ebaõnnestus",
+    });
+  }
 });
 
 export default router;
