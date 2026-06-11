dead2041e77bc93d13997b5b79c4287ba2dcaa60
lunarmagnoly <mari.luna.tei@outlook.com>
2026-06-11 18:53:37 +0300

    Implement POST task endpoint

--- server/routes/tasks.ts
+++ server/routes/tasks.ts
@@ -3,6 +3,7 @@ import sql from "mssql/msnodesqlv8";
 
 const router = Router();
 
+//select
 router.get("/", async (req, res) => {
   try {
     const result = await sql.query("SELECT * FROM Tasks");
@@ -13,6 +14,40 @@ router.get("/", async (req, res) => {
 
     res.status(500).json({
       message: "Andmete laadimine ebaõnnestus",
+    });
+  }
+});
+
+//insert
+router.post("/", async (req, res) => {
+  try {
+    const { title, description, status, priority, dueDate } = req.body;
+
+    await sql.query`
+      INSERT INTO Tasks (
+        Title,
+        Description,
+        Status,
+        Priority,
+        DueDate
+      )
+      VALUES (
+        ${title},
+        ${description},
+        ${status},
+        ${priority},
+        ${dueDate}
+      )
+    `;
+
+    res.status(201).json({
+      message: "Task lisatud",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Taski lisamine ebaõnnestus",
     });
   }
 });
